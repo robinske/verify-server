23f895b77dd63c839fe45e3dfe49431d2690eb9b
Kelley Robinson <krobinson@twilio.com>
2019-10-02 11:54:12 -0700

    Use verify v2

--- app_config.py
+++ app_config.py
@@ -3,4 +3,6 @@ from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
-AUTHY_API_KEY = os.environ.get('AUTHY_API_KEY')
+ACCOUNT_SID = os.environ.get('TWILIO_ACCOUNT_SID')
+AUTH_TOKEN = os.environ.get('TWILIO_AUTH_TOKEN')
+SERVICE_SID = os.environ.get('VERIFY_SERVICE_SID')
